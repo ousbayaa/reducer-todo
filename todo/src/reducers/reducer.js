f0cdd2fef7ca662e70b5d92db5984188155d7d20
Ous Bayaa <bayaaous@gmail.com>
2020-03-16 19:26:08 -0700

    added initial state

---
+++ todo/src/reducers/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = [
+    {
+        editing:false,
+        title: 'learn daily lessons'
+        id: 123
+    },
+    {
+        editing: false,
+        title: 'complete daily project'
+        id: 456
+    }
+]
